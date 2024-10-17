3a25d0e827ec51e495762c3a74101f34c5669db1
vacmg <70143287+vacmg@users.noreply.github.com>
2024-10-17 14:04:33 +0200

    Finish LocalCANNetwork_Tests

--- Tests/DoCANCppLibTests/LocalCANNetwork_TestUtils_Test.cpp
+++ Tests/DoCANCppLibTests/LocalCANNetwork_TestUtils_Test.cpp
@@ -141,6 +141,8 @@ TEST(LocalCANNetwork, network_read_write_peek_available_1_to_N_node)
 
         ASSERT_EQ(network.peekFrame(id, &fr), false);
         ASSERT_EQ(network.readFrame(id, &fr), false);
+        ASSERT_EQ(network.frameAvailable(id), 0);
+
         delete can;
     }
 }
@@ -159,16 +161,6 @@ TEST(LocalCANNetwork, network_active_test_1_node)
     delete can;
 }
 
-TEST(LocalCANNetwork, network_active_test_2_nodes)
-{
-    LocalCANNetwork network;
-    CANShim* can1 = network.newCANShimConnection();
-    CANShim* can2 = network.newCANShimConnection();
-    ASSERT_EQ(network.active(), true);
-    delete can1;
-    delete can2;
-}
-
 TEST(LocalCANNetwork, network_active_test_N_nodes)
 {
     uint32_t N = 100;
@@ -184,6 +176,119 @@ TEST(LocalCANNetwork, network_active_test_N_nodes)
         {
             ASSERT_EQ(network.active(), true);
         }
+        delete can;
+    }
+}
+
+TEST(LocalCANNetwork, CANShim_active_test_1)
+{
+    LocalCANNetwork network;
+    CANShim* can = network.newCANShimConnection();
+    ASSERT_NE(can, nullptr);
+    ASSERT_EQ(can->active(), false);
+    delete can;
+}
+
+TEST(LocalCANNetwork, CANShim_active_test_N)
+{
+    uint32_t N = 100;
+    LocalCANNetwork network;
+    CANShim* can1 = network.newCANShimConnection();
+    ASSERT_NE(can1, nullptr);
+    for (uint32_t i = 0; i < N; i++)
+    {
+        CANShim* can = network.newCANShimConnection();
+        ASSERT_NE(can, nullptr);
+        if(i < 1)
+        {
+            ASSERT_EQ(can->active(), false);
+            ASSERT_EQ(can1->active(), false);
+        }
+        else
+        {
+            ASSERT_EQ(can->active(), true);
+            ASSERT_EQ(can1->active(), true);
+        }
+        delete can;
+    }
+}
+
+TEST(LocalCANNetwork, CANShim_read_write_peek_available_1_node)
+{
+    LocalCANNetwork network;
+    LocalCANNetworkCANShim* can = network.newCANShimConnection();
+    ASSERT_NE(can, nullptr);
+    CANFrame frame;
+    frame.identifier.N_TAtype = CAN_CLASSIC_29bit_Physical;
+    frame.identifier.N_SA = 0;
+    frame.identifier.N_TA = 1;
+    frame.data_length_code = 8;
+    for (uint32_t i = 0; i < 8; i++)
+    {
+        frame.data[i] = i;
+    }
+
+    ASSERT_EQ(can->active(), false);
+    ASSERT_EQ(can->writeFrame(&frame), false);
+    ASSERT_EQ(can->frameAvailable(), 0);
+    ASSERT_EQ(can->readFrame(&frame), false);
+
+    delete can;
+}
+
+TEST(LocalCANNetwork, CANShim_read_write_peek_available_1_to_N_node)
+{
+    uint32_t N = 100;
+    LocalCANNetwork network;
+    LocalCANNetworkCANShim* wcan = network.newCANShimConnection();
+    ASSERT_NE(wcan, nullptr);
+    CANFrame frame;
+    frame.identifier.N_TAtype = CAN_CLASSIC_29bit_Physical;
+    frame.identifier.N_SA = 0;
+    frame.identifier.N_TA = 1;
+    frame.data_length_code = 8;
+    frame.flags = 1234;
+    for (uint32_t i = 0; i < 8; i++)
+    {
+        frame.data[i] = i;
+    }
+
+    ASSERT_EQ(wcan->active(), false);
+
+    std::list<LocalCANNetworkCANShim*> rcan;
+    for(int i = 1; i<N; i++)
+    {
+        rcan.push_front(network.newCANShimConnection());
+        ASSERT_NE(rcan.front(), nullptr);
+    }
+
+    CANFrame fr;
+
+    ASSERT_EQ(wcan->active(), true);
+
+    ASSERT_EQ(wcan->writeFrame(&frame), true);
+    ASSERT_EQ(wcan->frameAvailable(), 0);
+
+    ASSERT_EQ(wcan->writeFrame(&frame), true);
+    ASSERT_EQ(wcan->frameAvailable(), 0);
+
+    ASSERT_EQ(wcan->readFrame(&fr), false);
+
+    for(auto can : rcan)
+    {
+        uint32_t id = can->getNodeID();
+        ASSERT_EQ(can->frameAvailable(), 2);
+
+        ASSERT_EQ(can->readFrame(&fr), true);
+        ASSERT_EQ_FRAMES(frame, fr);
+        ASSERT_EQ(can->frameAvailable(), 1);
+
+        ASSERT_EQ(can->readFrame(&fr), true);
+        ASSERT_EQ_FRAMES(frame, fr);
+        ASSERT_EQ(can->frameAvailable(), 0);
+
+        ASSERT_EQ(can->readFrame(&fr), false);
+        ASSERT_EQ(can->frameAvailable(), 0);
         delete can;
     }
 }
