461c1dfde1203068a3b17ddce164817c85472b3d
vacmg <70143287+vacmg@users.noreply.github.com>
2024-10-17 09:45:07 +0200

    Change LocalCANNetworkTests to use ASSERT instead of expect to reduce verbosity

--- Tests/DoCANCppLibTests/LocalCANNetwork_TestUtils_Test.cpp
+++ Tests/DoCANCppLibTests/LocalCANNetwork_TestUtils_Test.cpp
@@ -1,13 +1,13 @@
 #include "gtest/gtest.h"
 #include "LocalCANNetwork.h"
 
-#define EXPECT_EQ_FRAMES(frame1, frame2) \
-    EXPECT_EQ(frame1.identifier.N_AI, frame2.identifier.N_AI); \
-    EXPECT_EQ(frame1.flags, frame2.flags); \
-    EXPECT_EQ(frame1.data_length_code, frame2.data_length_code); \
+#define ASSERT_EQ_FRAMES(frame1, frame2) \
+    ASSERT_EQ(frame1.identifier.N_AI, frame2.identifier.N_AI); \
+    ASSERT_EQ(frame1.flags, frame2.flags); \
+    ASSERT_EQ(frame1.data_length_code, frame2.data_length_code); \
     for (uint32_t i = 0; i < frame1.data_length_code; i++) \
     { \
-        EXPECT_EQ(frame1.data[i], frame2.data[i]); \
+        ASSERT_EQ(frame1.data[i], frame2.data[i]); \
     }
 
 TEST(LocalCANNetwork, network_newCANShimConnection_test)
@@ -15,7 +15,7 @@ TEST(LocalCANNetwork, network_newCANShimConnection_test)
     LocalCANNetwork network;
     LocalCANNetworkCANShim* can = network.newCANShimConnection();
     ASSERT_NE(can, nullptr);
-    EXPECT_EQ(can->getNodeID(), 0);
+    ASSERT_EQ(can->getNodeID(), 0);
     delete can;
 }
 
@@ -27,7 +27,7 @@ TEST(LocalCANNetwork, network_newCANShimConnection_test_N_nodes)
     {
         LocalCANNetworkCANShim* can = network.newCANShimConnection();
         ASSERT_NE(can, nullptr);
-        EXPECT_EQ(can->getNodeID(), i);
+        ASSERT_EQ(can->getNodeID(), i);
         delete can;
     }
 }
@@ -46,11 +46,11 @@ TEST(LocalCANNetwork, network_read_write_peek_available_0_node)
         frame.data[i] = i;
     }
 
-    EXPECT_EQ(network.active(), false);
-    EXPECT_EQ(network.writeFrame(id, &frame), false);
-    EXPECT_EQ(network.frameAvailable(id), 0);
-    EXPECT_EQ(network.readFrame(id, &frame), false);
-    EXPECT_EQ(network.peekFrame(id, &frame), false);
+    ASSERT_EQ(network.active(), false);
+    ASSERT_EQ(network.writeFrame(id, &frame), false);
+    ASSERT_EQ(network.frameAvailable(id), 0);
+    ASSERT_EQ(network.readFrame(id, &frame), false);
+    ASSERT_EQ(network.peekFrame(id, &frame), false);
 }
 
 TEST(LocalCANNetwork, network_read_write_peek_available_1_node)
@@ -69,11 +69,11 @@ TEST(LocalCANNetwork, network_read_write_peek_available_1_node)
         frame.data[i] = i;
     }
 
-    EXPECT_EQ(network.active(), false);
-    EXPECT_EQ(network.writeFrame(id, &frame), false);
-    EXPECT_EQ(network.frameAvailable(id), 0);
-    EXPECT_EQ(network.readFrame(id, &frame), false);
-    EXPECT_EQ(network.peekFrame(id, &frame), false);
+    ASSERT_EQ(network.active(), false);
+    ASSERT_EQ(network.writeFrame(id, &frame), false);
+    ASSERT_EQ(network.frameAvailable(id), 0);
+    ASSERT_EQ(network.readFrame(id, &frame), false);
+    ASSERT_EQ(network.peekFrame(id, &frame), false);
 
     delete can;
 }
@@ -96,7 +96,7 @@ TEST(LocalCANNetwork, network_read_write_peek_available_1_to_N_node)
         frame.data[i] = i;
     }
 
-    EXPECT_EQ(network.active(), false);
+    ASSERT_EQ(network.active(), false);
 
     std::list<LocalCANNetworkCANShim*> rcan;
     for(int i = 1; i<N; i++)
@@ -107,40 +107,40 @@ TEST(LocalCANNetwork, network_read_write_peek_available_1_to_N_node)
 
     CANFrame fr;
 
-    EXPECT_EQ(network.active(), true);
+    ASSERT_EQ(network.active(), true);
 
-    EXPECT_EQ(network.writeFrame(writterId, &frame), true);
-    EXPECT_EQ(network.frameAvailable(writterId), 0);
+    ASSERT_EQ(network.writeFrame(writterId, &frame), true);
+    ASSERT_EQ(network.frameAvailable(writterId), 0);
 
-    EXPECT_EQ(network.writeFrame(writterId, &frame), true);
-    EXPECT_EQ(network.frameAvailable(writterId), 0);
+    ASSERT_EQ(network.writeFrame(writterId, &frame), true);
+    ASSERT_EQ(network.frameAvailable(writterId), 0);
 
-    EXPECT_EQ(network.peekFrame(writterId, &fr), false);
-    EXPECT_EQ(network.readFrame(writterId, &fr), false);
-    EXPECT_EQ(network.peekFrame(writterId, &fr), false);
+    ASSERT_EQ(network.peekFrame(writterId, &fr), false);
+    ASSERT_EQ(network.readFrame(writterId, &fr), false);
+    ASSERT_EQ(network.peekFrame(writterId, &fr), false);
 
     for(auto can : rcan)
     {
         uint32_t id = can->getNodeID();
-        EXPECT_EQ(network.frameAvailable(id), 2);
-        EXPECT_EQ(network.peekFrame(id, &fr), true);
-        EXPECT_EQ_FRAMES(frame, fr);
-        EXPECT_EQ(network.frameAvailable(id), 2);
+        ASSERT_EQ(network.frameAvailable(id), 2);
+        ASSERT_EQ(network.peekFrame(id, &fr), true);
+        ASSERT_EQ_FRAMES(frame, fr);
+        ASSERT_EQ(network.frameAvailable(id), 2);
 
-        EXPECT_EQ(network.readFrame(id, &fr), true);
-        EXPECT_EQ_FRAMES(frame, fr);
-        EXPECT_EQ(network.frameAvailable(id), 1);
+        ASSERT_EQ(network.readFrame(id, &fr), true);
+        ASSERT_EQ_FRAMES(frame, fr);
+        ASSERT_EQ(network.frameAvailable(id), 1);
 
-        EXPECT_EQ(network.peekFrame(id, &fr), true);
-        EXPECT_EQ_FRAMES(frame, fr);
-        EXPECT_EQ(network.frameAvailable(id), 1);
+        ASSERT_EQ(network.peekFrame(id, &fr), true);
+        ASSERT_EQ_FRAMES(frame, fr);
+        ASSERT_EQ(network.frameAvailable(id), 1);
 
-        EXPECT_EQ(network.readFrame(id, &fr), true);
-        EXPECT_EQ_FRAMES(frame, fr);
-        EXPECT_EQ(network.frameAvailable(id), 0);
+        ASSERT_EQ(network.readFrame(id, &fr), true);
+        ASSERT_EQ_FRAMES(frame, fr);
+        ASSERT_EQ(network.frameAvailable(id), 0);
 
-        EXPECT_EQ(network.peekFrame(id, &fr), false);
-        EXPECT_EQ(network.readFrame(id, &fr), false);
+        ASSERT_EQ(network.peekFrame(id, &fr), false);
+        ASSERT_EQ(network.readFrame(id, &fr), false);
         delete can;
     }
 }
@@ -148,14 +148,14 @@ TEST(LocalCANNetwork, network_read_write_peek_available_1_to_N_node)
 TEST(LocalCANNetwork, active_test_0_nodes)
 {
     LocalCANNetwork network;
-    EXPECT_EQ(network.active(), false);
+    ASSERT_EQ(network.active(), false);
 }
 
 TEST(LocalCANNetwork, network_active_test_1_node)
 {
     LocalCANNetwork network;
     CANShim* can = network.newCANShimConnection();
-    EXPECT_EQ(network.active(), false);
+    ASSERT_EQ(network.active(), false);
     delete can;
 }
 
@@ -164,7 +164,7 @@ TEST(LocalCANNetwork, network_active_test_2_nodes)
     LocalCANNetwork network;
     CANShim* can1 = network.newCANShimConnection();
     CANShim* can2 = network.newCANShimConnection();
-    EXPECT_EQ(network.active(), true);
+    ASSERT_EQ(network.active(), true);
     delete can1;
     delete can2;
 }
@@ -178,11 +178,11 @@ TEST(LocalCANNetwork, network_active_test_N_nodes)
         CANShim* can = network.newCANShimConnection();
         if(i < 2)
         {
-            EXPECT_EQ(network.active(), false);
+            ASSERT_EQ(network.active(), false);
         }
         else
         {
-            EXPECT_EQ(network.active(), true);
+            ASSERT_EQ(network.active(), true);
         }
         delete can;
     }
