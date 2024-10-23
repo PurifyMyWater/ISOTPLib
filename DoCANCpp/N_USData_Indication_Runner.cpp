f0bb2348f6d758b66188a4e9ceed8b087260e0f0
vacmg <70143287+vacmg@users.noreply.github.com>
2024-10-23 08:42:11 +0200

    create skeleton for Runners

--- DoCANCpp/N_USData_Indication_Runner.cpp
+++ DoCANCpp/N_USData_Indication_Runner.cpp
@@ -1,2 +1,16 @@
 #include "N_USData_Indication_Runner.h"
 
+N_USData_Indication_Runner::N_USData_Indication_Runner(N_AI nAi, uint32_t* availableMemoryForRunners, uint8_t blockSize, STmin stMin, OSShim* osShim, CANShim* canShim): N_USData_Runner(nAi, osShim, canShim)
+{
+
+}
+
+N_USData_Indication_Runner::~N_USData_Indication_Runner()
+{
+
+}
+
+N_Result N_USData_Indication_Runner::run_step(CANFrame* frame)
+{
+    return N_ERROR;
+}
--- DoCANCpp/N_USData_Request_Runner.cpp
+++ DoCANCpp/N_USData_Request_Runner.cpp
@@ -1,3 +1,17 @@
 #include "N_USData_Request_Runner.h"
 
 
+N_USData_Request_Runner::N_USData_Request_Runner(N_AI nAi, Mtype mType, uint8_t* messageData, uint32_t messageLength, OSShim* osShim, CANShim* canShim) : N_USData_Runner(nAi, osShim, canShim)
+{
+
+}
+
+N_USData_Request_Runner::~N_USData_Request_Runner()
+{
+
+}
+
+N_Result N_USData_Request_Runner::run_step(CANFrame* frame)
+{
+    return N_ERROR;
+}
