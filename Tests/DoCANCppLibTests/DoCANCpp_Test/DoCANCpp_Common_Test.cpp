1b4299e2626e29e25fb13c5adaade8a454b6ce98
vacmg <70143287+vacmg@users.noreply.github.com>
2025-03-15 17:23:30 +0100

    Rename N_ResultToString tests
    Took 43 minutes

--- Tests/DoCANCppLibTests/DoCANCpp_Test/DoCANCpp_Common_Test.cpp
+++ Tests/DoCANCppLibTests/DoCANCpp_Test/DoCANCpp_Common_Test.cpp
@@ -1,21 +1,21 @@
 #include "DoCANCpp_Common.h"
 #include <gtest/gtest.h>
 
-TEST(DoCANCpp_Common, N_Result_to_string)
+TEST(DoCANCpp_Common, N_ResultToString)
 {
-    EXPECT_STREQ("NOT_STARTED", N_Result_to_string(NOT_STARTED));
-    EXPECT_STREQ("IN_PROGRESS", N_Result_to_string(IN_PROGRESS));
-    EXPECT_STREQ("N_OK", N_Result_to_string(N_OK));
-    EXPECT_STREQ("N_TIMEOUT_A", N_Result_to_string(N_TIMEOUT_A));
-    EXPECT_STREQ("N_TIMEOUT_Bs", N_Result_to_string(N_TIMEOUT_Bs));
-    EXPECT_STREQ("N_TIMEOUT_Cr", N_Result_to_string(N_TIMEOUT_Cr));
-    EXPECT_STREQ("N_WRONG_SN", N_Result_to_string(N_WRONG_SN));
-    EXPECT_STREQ("N_INVALID_FS", N_Result_to_string(N_INVALID_FS));
-    EXPECT_STREQ("N_UNEXP_PDU", N_Result_to_string(N_UNEXP_PDU));
-    EXPECT_STREQ("N_WFT_OVRN", N_Result_to_string(N_WFT_OVRN));
-    EXPECT_STREQ("N_BUFFER_OVFLW", N_Result_to_string(N_BUFFER_OVFLW));
-    EXPECT_STREQ("N_ERROR", N_Result_to_string(N_ERROR));
-    EXPECT_STREQ("UNKNOWN", N_Result_to_string(static_cast<N_Result>(999)));
+    EXPECT_STREQ("NOT_STARTED", N_ResultToString(NOT_STARTED));
+    EXPECT_STREQ("IN_PROGRESS", N_ResultToString(IN_PROGRESS));
+    EXPECT_STREQ("N_OK", N_ResultToString(N_OK));
+    EXPECT_STREQ("N_TIMEOUT_A", N_ResultToString(N_TIMEOUT_A));
+    EXPECT_STREQ("N_TIMEOUT_Bs", N_ResultToString(N_TIMEOUT_Bs));
+    EXPECT_STREQ("N_TIMEOUT_Cr", N_ResultToString(N_TIMEOUT_Cr));
+    EXPECT_STREQ("N_WRONG_SN", N_ResultToString(N_WRONG_SN));
+    EXPECT_STREQ("N_INVALID_FS", N_ResultToString(N_INVALID_FS));
+    EXPECT_STREQ("N_UNEXP_PDU", N_ResultToString(N_UNEXP_PDU));
+    EXPECT_STREQ("N_WFT_OVRN", N_ResultToString(N_WFT_OVRN));
+    EXPECT_STREQ("N_BUFFER_OVFLW", N_ResultToString(N_BUFFER_OVFLW));
+    EXPECT_STREQ("N_ERROR", N_ResultToString(N_ERROR));
+    EXPECT_STREQ("UNKNOWN", N_ResultToString(static_cast<N_Result>(999)));
 }
 
 TEST(DoCANCpp_Common, getStMinInMs)
