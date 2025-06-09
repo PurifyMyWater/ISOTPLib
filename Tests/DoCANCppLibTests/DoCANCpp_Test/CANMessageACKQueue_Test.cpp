e44d81f3395de7b792dd7c035a2f3bc2b4b4bacf
vacmg <70143287+vacmg@users.noreply.github.com>
2025-06-10 00:33:02 +0200

    Add removeFromQueue test

--- Tests/DoCANCppLibTests/DoCANCpp_Test/CANMessageACKQueue_Test.cpp
+++ Tests/DoCANCppLibTests/DoCANCpp_Test/CANMessageACKQueue_Test.cpp
@@ -11,6 +11,48 @@
 
 static LinuxOSInterface linuxOSInterface;
 
+TEST(CANMessageACKQueue, removeFromQueue)
+{
+    // Given
+    LocalCANNetwork    localCANNetwork;
+    CANInterface*      canInterface = localCANNetwork.newCANInterfaceConnection();
+    CANMessageACKQueue canMessageACKQueue(*canInterface, linuxOSInterface);
+    CANFrame           frame  = NewCANFrameDoCANCpp();
+    CANFrame           frame2 = NewCANFrameDoCANCpp();
+
+    // Create dumb runner
+    int64_t                 availableMemoryConst = 100;
+    Atomic_int64_t          availableMemoryMock(availableMemoryConst, linuxOSInterface);
+    N_AI                    NAi               = DoCANCpp_N_AI_CONFIG(N_TATYPE_6_CAN_CLASSIC_29bit_Functional, 1, 2);
+    const char*             testMessageString = ""; // strlen = 0
+    size_t                  messageLen        = strlen(testMessageString);
+    const uint8_t*          testMessage       = reinterpret_cast<const uint8_t*>(testMessageString);
+    bool                    result;
+    N_USData_Request_Runner runner(result, NAi, availableMemoryMock, Mtype_Diagnostics, testMessage, messageLen,
+                                   linuxOSInterface, canMessageACKQueue);
+
+    for (int i = 0; i < 8; i++)
+    {
+        frame.data[i] = i;
+    }
+
+    for (int i = 0; i < 8; i++)
+    {
+        frame2.data[i] = i + 8;
+    }
+
+    CANInterface* receivedCanInterface = localCANNetwork.newCANInterfaceConnection();
+
+    ASSERT_TRUE(canMessageACKQueue.writeFrame(runner, frame));
+    ASSERT_TRUE(canMessageACKQueue.writeFrame(runner, frame2));
+
+    EXPECT_TRUE(canMessageACKQueue.removeFromQueue(NAi));
+    EXPECT_FALSE(canMessageACKQueue.removeFromQueue(NAi));
+
+    delete receivedCanInterface;
+    delete canInterface;
+}
+
 TEST(CANMessageACKQueue, writeFrame)
 {
     // Given
